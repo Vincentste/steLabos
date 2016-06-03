1a117b1cf1ca02bc37cab65dd05c9a29d0c739c4
internet10 <internet10>
2016-06-03 14:02:38 +0200

    template

---
+++ LesBluesBrothersLabo/c/templateRecherche.php
@@ -0,0 +1,24 @@
+//template recherche
+<div id='champHaut'>
+   <h1>Moteur de recherche</h1>
+    <input type='text' id='champTexte'/>
+</div>
+<div id='champbas'>
+   <p>
+    <label for="createurs">Créateurs</label>
+    <select id="selectCre" name="createurs" class=""></select>
+</p>    
+<p>
+    <label for="matieres">Matières</label>
+    <select id="selectMat" name="matieres" class=''></select>
+</p>  
+<p>
+    <label for="catégories">Catégories</label>
+    <select id="selectCat" name="categorie" class=''></select>
+</p>  
+<p>
+    <input type="button" id='boutChampRech' value="envoyer" autocomplete="off"/>
+</p>
+    
+</div>
+
