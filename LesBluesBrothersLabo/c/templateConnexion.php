a976395ae4905f7202fc0bd6c8ea36094ac42845
internet07 <internet07>
2016-06-13 08:41:16 +0200

    g

---
+++ LesBluesBrothersLabo/c/templateConnexion.php
@@ -0,0 +1,207 @@
+<?php
+
+class Affichage{
+     
+   public function afficheConnexion(){
+        return '<div id="formConnexion">
+         <form class="connexion"><label for="nom">Nom</label>        
+            <input type="text" name="nom"id="nom" autocomplete="off" />
+            </br>
+            <label for="mdp">password</label>
+            <input type="password" name="mdp" id="mdp" autocomplete="off"/>
+            </br>
+            <input type="button" class="envoi" value="envoyer" autocomplete="off"/>
+         
+        <form/>
+        </div>';
+    }
+    
+    public function afficheChampsRecherche(){
+        return '
+            <section class="content">
+            <h1>Moteur de recherche</h1>
+            <form><fieldset id="champHaut">  
+            <input type="text" id="champTexte" name="champTexte"/>
+            </fieldset>
+            <fieldset id="champbas">
+            <p>
+            <label for="createurs">Créateurs : </label>
+            <select id="selectCre" name="createurs" class=""></select>
+            </p> 
+            <p>
+            <label for="matieres">Matières : </label>
+            <select id="selectMat" name="matieres" class=""></select>
+            </p>
+            <p>
+            <label for="categories">Catégories : </label>
+            <select id="selectCat" name="categories" class=""></select>
+            </p>           
+            <input type="button" id="boutChampRech" value="Afficher" autocomplete="off"/>
+            </p>   
+            </fieldset></form>
+            
+            <div id="divAjout"> 
+                <div id="buttonAjout"> Ajouter un nouveau t-shirt </div>
+                <div id="voletAjout"></div>
+            </div>
+            </section>'; 
+    }
+
+    public function afficheAjoutTshirt(){
+        return '
+        <form>
+            <section id="imgPrew">
+                <figure>
+                    <img src="images/no.jpg" alt="image liste preview" />
+                    <figcaption>Image liste</figcaption>
+                </figure>
+                <figure>
+                     <img src="images/no.jpg" alt="image détails preview"/>
+                     <figcaption>Image détails</figcaption>
+                </figure>
+            </section>
+            <section id="infosAjout">  
+                    <p>
+                        <label for="prodNomAjout">Nom : </label>
+                        <input type="text" name="prodNomAjout" id="prodNomAjout"/>
+                    </p>
+                    <p>
+                        <label for="prodPrixAjout">Prix : </label>
+                        <input type="text" name="prodPrixAjout" id="prodPrixAjout" />
+                    </p>
+                    <p>
+                        <label for="prodDateAjout">Date : </label>
+                        <input type="date" name="prodDateAjout" id="prodDateAjout" />
+                    </p>
+                    <p>
+                        <label for="prodDescAjout">Description : </label>
+                        <textarea name="prodDescAjout" id="prodDescAjout"></textarea>
+                    </p>
+
+                    <p>
+                        <label for="prodCreAjout">Créateurs : </label>
+                        <select id="prodCreAjout" name="prodCreAjout"></select>
+                    </p> 
+                    <p>
+                        <label for="prodMatAjout">Matières : </label>
+                        <select id="prodMatAjout" name="prodMatAjout"></select>
+                    </p>
+                    <p>
+                        <label for="prodCatAjout">Catégories : </label>
+                        <select id="prodCatAjout" name="prodCatAjout"></select>
+                    </p>
+                
+            </section>
+            <section id="taillesAjout">
+                <h2>Tailles</h2> 
+                <p>
+                    <label for="tailleS">S : </label>
+                    <input type="text" name="tailleS" id="tailleS"/> <span class="fa fa-trash"></span> <span class="fa fa-pencil"></span>
+                </p>
+                <p>
+                    <label for="tailleM">M : </label>
+                    <input type="text" name="tailleM" id="tailleM"/> <span class="fa fa-trash"></span> <span class="fa fa-pencil"></span>
+                </p>
+                <p>
+                    <label for="tailleL">L : </label>
+                    <input type="text" name="tailleL" id="tailleL"/> <span class="fa fa-trash"></span> <span class="fa fa-pencil"></span>
+                </p>
+                <p>
+                    <label for="tailleXL">XL : </label>
+                    <input type="text" name="tailleXL" id="tailleXL"/> <span class="fa fa-trash"></span> <span class="fa fa-pencil"></span>
+                </p>
+                <input type="button" id="ajoutTaille" value="ajouter" autocomplete="off"/>
+            </section>
+            <div class="boutonsFormAjout">
+                <input type="button" id="boutSav" value="Sauvegarder" autocomplete="off"/>
+                <input type="button" id="boutAnn" value="Annuler" autocomplete="off"/>
+            </div>
+        </form>
+        <section id="imageAjout">
+            <h2>IMAGES</h2>
+            <div id="voletImageAjout">
+                
+            </div>
+        </section>';
+    }
+
+    public function afficheModifTshirt(){
+        return '
+        
+             <section id="imgPrew">
+                <figure>
+                    <img  id="imglist" src="" alt="image liste preview" />
+                    <figcaption>Image liste</figcaption>
+                </figure>
+                <figure>
+                     <img id="imgprew" src="" alt="image détails preview"/>
+                     <figcaption>Image détails</figcaption>
+                </figure>
+            </section>
+            <section id="infosAjout">  
+                 <br/>
+                        <label for="prodNom">Nom : </label>
+                        <input type="text" name="prodNom" id="prodNom"/>
+                    <br/>
+                   <br/>
+                        <label for="prodPrix">Prix : </label>
+                        <input type="text" name="prodPrix" id="prodPrix" />
+                    <br/>
+                   <br/>
+                        <label for="prodDate">Date : </label>
+                        <input type="date" name="prodDate" id="prodDate" />
+                    <br/>
+                   <br/>
+                        <label for="prodDesc">Description : </label>
+                        <textarea name="prodDesc" id="prodDesc"></textarea>
+                    <br/>
+
+                   <br/>
+                        <label for="prodCre">Créateurs : </label>
+                        <select id="prodCre" name="prodCre"></select>
+                    <br/> 
+                   <br/>
+                        <label for="prodMat">Matières : </label>
+                        <select id="prodMat" name="prodMat"></select>
+                    <br/>
+                   <br/>
+                        <label for="prodCat">Catégories : </label>
+                        <select id="prodCat" name="prodCat"></select>
+                    <br/>
+                
+            </section>
+            <section id="taillesAjout">
+                <h2>Tailles</h2> 
+               <br/>
+                    <label for="tailleS">S : </label>
+                    <input type="text" name="tailleS" id="ModifTailleS"/> <span data="ModifTailleS" id="1" class="suppTaille fa fa-trash"></span> <span class="fa fa-pencil"></span>
+                <br/>
+               <br/>
+                    <label for="tailleM">M : </label>
+                    <input type="text" name="tailleM" id="ModifTailleM"/> <span data="ModifTailleM" id="2" class="suppTaille fa fa-trash"></span> <span class="fa fa-pencil"></span>
+                <br/>
+               <br/>
+                    <label for="tailleL">L : </label>
+                    <input type="text" name="tailleL" id="ModifTailleL"/> <span data="ModifTailleL" id="3" class="suppTaille fa fa-trash"></span> <span class="fa fa-pencil"></span>
+                <br/>
+               <br/>
+                    <label for="tailleXL">XL : </label>
+                    <input type="text" name="tailleXL" id="ModifTailleXL"/> <span data="ModifTailleXL" id="4" class="suppTaille fa fa-trash"></span> <span class="fa fa-pencil"></span>
+                <br/>
+                <input type="button" id="ajoutTaille" value="ajouter" autocomplete="off"/>
+            </section>
+            <div class="boutonsFormModifier">
+                <input type="button" id="boutMod" value="Sauvegarder" autocomplete="off"/>
+                <input type="button" id="boutAnnMod" value="Annuler" autocomplete="off"/>
+            </div>
+        ';
+    }
+    
+    
+
+
+}
+   
+
+?>
+   
---
+++ LesBluesBrothersLabo/m/connexion.php
@@ -0,0 +1,179 @@
+<?php
+function connexion_PDO() {
+		static $connexion;
+		if (!isset ($connexion)) {
+			$utilisateur = "labo";
+			$motdepasse = "labo";
+			$serveur= "mysql:host=localhost;dbname=tshirt";
+			$connexion = new PDO ($serveur,$utilisateur,$motdepasse);
+		 	$connexion->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$requete="SET NAMES utf8";
+			$resultat= $connexion->exec($requete);
+ 	  	}
+ 	  	return ($connexion);
+ 	}
+
+
+function requeteConnexion($nom,$mdp){
+	$req = "SELECT admin_id 
+	FROM administrateurs
+	WHERE admin_login = :login AND admin_pwd = :mdp";
+	$connexion = connexion_PDO();
+	$res = $connexion->prepare($req);
+	$res->execute([":login"=>$nom,
+				   ":mdp"=>$mdp]);
+    //renvoi vrai si !=0,faux sinon
+	return $res->fetch(PDO::FETCH_OBJ)!=null;
+}
+
+function recupAllCreateurs(){
+	$requete="SELECT * FROM createurs";
+	$connexion=connexion_PDO();
+	//preparation requete
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute();
+    //dans résultat on a l'ensemble des résultats de la requete
+     return $resultat->fetchAll(PDO::FETCH_OBJ);
+}
+
+function recupAllMatieres(){
+	$requete="SELECT * FROM matieres";
+	$connexion=connexion_PDO();
+	//preparation requete
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute();
+    //dans résultat on a l'ensemble des résultats de la requete
+     return $resultat->fetchAll(PDO::FETCH_OBJ);
+}
+
+function recupAllCategories(){
+	$requete="SELECT * FROM categories";
+	$connexion=connexion_PDO();
+	//preparation requete
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute();
+    //dans résultat on a l'ensemble des résultats de la requete
+     return $resultat->fetchAll(PDO::FETCH_OBJ);
+}
+	
+function requeteTshirtParNoms($lettre){
+	$requete='SELECT * FROM produits where prod_nom like :a';
+	$connexion=connexion_PDO();
+	//preparation requete
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute([':a'=>"%".$lettre."%"]);
+    //dans résultat on a l'ensemble des résultats de la requete
+     return $resultat->fetchAll(PDO::FETCH_OBJ);
+}
+
+function requeteSupprimerTshirt($id){
+	$requete='DELETE produits,exemplaires FROM produits  left join exemplaires ON exem_fk_tee = prod_id where prod_id = :a';
+	$connexion=connexion_PDO();
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute([':a'=>$id]);
+}
+
+function requeteInsertTshirt($nom,$prix,$img_gd,$img_pt,$des,$crea,$mat,$date_aj,$cat){
+	$requete='INSERT INTO produits(prod_nom,prod_prix,prod_img_gd,prod_img_pt,prod_desc,prod_fk_createur,prod_fk_matiere,prod_date,prod_fk_categorie) VALUES (:nom,:prix,:img_gd,:img_pt,:des,:crea,:mat,:date_aj,:cat)';
+	$connexion=connexion_PDO();
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute(
+    	[':nom'=>$nom,
+    	 ':prix'=>$prix,
+    	 ':img_gd'=>$img_gd,
+    	 ':img_pt'=>$img_pt,
+    	 ':des'=>$des,
+    	 ':crea'=>$crea,
+    	 ':mat'=>$mat,
+    	 ':date_aj'=>$date_aj,
+    	 ':cat'=>$cat
+    	]);
+     
+    return $connexion->lastInsertId("prod_id");
+}
+
+function requeteInsertExem($id_exem,$id_taille,$stock){
+    $requete='INSERT INTO exemplaires(exem_fk_tee,exem_fk_tail,exem_stock) VALUES (:id_exem,:id_taille,:stock)';
+    $connexion=connexion_PDO();
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute(
+        [':id_exem'=>$id_exem,
+         ':id_taille'=>$id_taille,
+         ':stock'=>$stock
+        ]);
+}
+
+  function requeteTshirtFiltres($categorie,$createur,$matiere){
+     $requete='Select * from produits'
+         .' JOIN categories on prod_fk_categorie = cat_id'
+         .' JOIN matieres on prod_fk_matiere = mat_id'
+         .' JOIN createurs on prod_fk_createur = cre_id'
+         .' Where cat_nom like :a'
+         .' and cre_nom like :b'
+         .' and mat_nom like :c';
+     $connexion=connexion_PDO();
+ 	//preparation requete
+     $resultat=$connexion->prepare($requete);
+     $resultat->execute([':a'=>$categorie,':b'=>$createur,':c'=>$matiere]);
+     return $resultat->fetchAll(PDO::FETCH_OBJ);
+  }
+
+function recupere_exemplaire($id,$taille){
+    $requete="select exem_stock FROM exemplaires "
+            . "JOIN produits ON prod_id=exem_fk_tee "
+            . "JOIN createurs on prod_fk_createur=cre_id "
+            . "JOIN matieres on prod_fk_matiere=mat_id "
+            . "JOIN categories on prod_fk_categorie=cat_id "
+            . "JOIN tailles ON exem_fk_tail=tail_id WHERE  tail_nom=:b AND prod_id=:a";
+  
+    $connexion=connexion_PDO();
+        //preparation requete
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute([':a'=>$id,':b'=>$taille]);
+    return $resultat->fetchAll(PDO::FETCH_OBJ);
+}
+
+function recupere_infos_untshirt($id){
+    $requete='Select * from produits'
+         .' JOIN categories on prod_fk_categorie = cat_id'
+         .' JOIN matieres on prod_fk_matiere = mat_id'
+         .' JOIN createurs on prod_fk_createur = cre_id'
+         .' LEFT OUTER JOIN exemplaires on prod_id = exem_fk_tee'
+         .' Where prod_id = :a';
+     $connexion=connexion_PDO();
+    //preparation requete
+     $resultat=$connexion->prepare($requete);
+     $resultat->execute([':a'=>$id]);
+     return $resultat->fetchAll(PDO::FETCH_OBJ);
+    
+}    
+
+//update (nom,prix,date,desc,crea,mat,cat) dans la DB d un t-shirt
+function RequeteUpdate_Tshirt($id,$nom,$prix,$date,$desc,$crea,$mat,$cat){
+    $requete='Update produits SET prod_nom = :nom ,prod_prix = :prix,prod_date = :date, prod_desc = :desc, prod_fk_createur = :crea, prod_fk_matiere = :mat, prod_fk_categorie = :cat WHERE prod_id = :id';
+    $connexion=connexion_PDO();
+    //preparation requete
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute([":id"=>$id,":nom"=>$nom,":prix"=>$prix,":date"=>$date,":desc"=>$desc,":crea"=>$crea,":mat"=>$mat,":cat"=>$cat]);
+
+ }
+
+//supprime le stock d'une taille d'un tshirt
+function  RequeteSupprimeStockTaille($idTaille,$idTshirt){
+    $requete='Update exemplaires SET exem_stock = null WHERE  exem_fk_tail= :idTaille AND exem_fk_tee = :idTshirt';
+    $connexion=connexion_PDO();
+    //preparation requete
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute([":idTaille"=>$idTaille,":idTshirt"=>$idTshirt]);
+}
+
+
+function requeteSelectImg(){
+    $requete='SELECT DISTINCT prod_img_gd,prod_img_pt FROM produits 
+              WHERE prod_img_gd !="" AND prod_img_pt !=""';
+    $connexion=connexion_PDO();
+    $resultat=$connexion->prepare($requete);
+    $resultat->execute();
+    return $resultat->fetchAll(PDO::FETCH_OBJ);
+}
+
